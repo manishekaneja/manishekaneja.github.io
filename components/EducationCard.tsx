bb990a4d1c624b57fffe39ca4107e974045ce723
Manish Aneja <manish.aneja@grofers.com>
2026-06-12 15:19:09 +0530

    Fix home a11y: education heading order and sidebar tap targets

--- components/EducationCard.tsx
+++ components/EducationCard.tsx
@@ -7,7 +7,7 @@ export default function EducationCard() {
   return (
     <div className="edu">
       <div className="gl">Education</div>
-      <h4>{education.degree}</h4>
+      <h3>{education.degree}</h3>
       <div className="sch">{education.school}</div>
       <div className="det">{education.detail}</div>
     </div>
